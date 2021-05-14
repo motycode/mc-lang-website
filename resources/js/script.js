fd87e7a017dba3dbbc7b77d0c73d7a853dcb7d93
MotyCode <motycode@gmail.com>
2021-05-14 17:38:38 +0200

    Implement smooth navigation.

---
+++ resources/js/script.js
@@ -0,0 +1,8 @@
+// smooth navigation
+document.querySelector(".nav").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
