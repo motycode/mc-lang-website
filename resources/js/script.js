d0503519d082cbb824f7f74fe9df4d48d86e6447
MotyCode <motycode@gmail.com>
2021-05-16 18:02:32 +0200

    Add sticky navigation.

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,3 +6,21 @@ document.querySelector(".nav").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// sticky navigation
+const header = document.getElementsByClassName("header")[0];
+const nav = document.getElementsByClassName("navigation")[0];
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("navigation--sticky");
+  } else {
+    nav.classList.remove("navigation--sticky");
+  }
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-70px",
+});
+headerObserver.observe(header);
