42d2a6f8211ccfe406c9475a796e5a56654340db
MotyCode <motycode@gmail.com>
2021-05-17 17:13:00 +0200

    Reveal sections on scroll.

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,3 +24,20 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: "-70px",
 });
 headerObserver.observe(header);
+
+// reveal sections on scroll
+const sections = document.querySelectorAll("section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+sections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
